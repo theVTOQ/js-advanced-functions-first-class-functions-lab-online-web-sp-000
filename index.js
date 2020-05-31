bbe74675b107d53811d156fc6bd6d07728df2728
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 21:10:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+  return [drivers[0], drivers[1]];
+}
+
+function returnLastTwoDrivers(drivers){
+  return [drivers[-2], drivers[-1]];
+}

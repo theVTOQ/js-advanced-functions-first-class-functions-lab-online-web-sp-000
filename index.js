04aef7f8959a9aa2ce727747b75b66acfce2242c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 21:29:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+  return [drivers[0], drivers[1]];
+}
+
+const returnLastTwoDrivers = function(drivers){
+  const length = drivers.length;
+  return [drivers[length - 2], drivers[length - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+  return function(fare){
+    return fare * multiplier;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnDriversFn){
+  return returnDriversFn(drivers);
+}

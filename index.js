3be7454d7f495f9fc6cf7af8200ff7a94ec00e3c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 21:13:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const returnLastTwoDrivers = function(drivers){
   const length = drivers.length;
   return [drivers[length - 2], drivers[length - 1]];
 }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
